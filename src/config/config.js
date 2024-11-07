397f21ecccbd5ce0e34a9d754af7a56b1e96cf26
오세연 <your_email@example.com>
2024-11-07 11:16:01 +0900

    basic

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,3 @@
 export const API_URL = 'https://api.themoviedb.org/3/';
 export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
 export const API_KEY = 'a788b64a1e85b53d1f19cb4b6b728a66'; 
-export const USER_SERVER = '/api/users'
