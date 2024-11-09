9029bcb87262696ae25ef9d0c865217f97d3b97e
오세연 <your_email@example.com>
2024-11-09 23:49:53 +0900

    everything

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,9 +14,8 @@ const Navbar = () => {
       <div className={`navbar__menu ${isMenuOpen ? 'navbar__menu--open' : ''}`}>
         <ul>
           <li>홈</li>
-          <li>시리즈</li>
-          <li>영화</li>
           <li>NEW! 요즘 대세 콘텐츠</li>
+          <li>찾아보기</li>
           <li>내가 찜한 리스트</li>
         </ul>
       </div>
