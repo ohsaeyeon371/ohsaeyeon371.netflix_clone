eb76854b686458fcb6bca34bd20fcaaa122a8ba5
오세연 <your_email@example.com>
2024-11-26 00:25:49 +0900

    master

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import axios from 'axios';
+// import axios from 'axios';
 import './Login.css';
 
 const Login = () => {
